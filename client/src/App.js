e75b98f6664a534db0046338b0ccb81b4d5102f2
Issac Moreno <issaxavier909@gmail.com>
2020-03-27 11:12:36 -0700

    added register to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Register from './Login_Register/Register';
+
 function App() {
   return (
     <div className="App">
-
+      <Register />
     </div>
   );
 }
